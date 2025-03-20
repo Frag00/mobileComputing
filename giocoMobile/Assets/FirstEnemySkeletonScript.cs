2f330170e654a22531af35b1aef8d1c2def58a10
Frag00 <miele2922@gmail.com>
2025-03-20 19:50:56 +0100

    Aggiunte : implementata logica attacco del nemico

--- giocoMobile/Assets/FirstEnemySkeletonScript.cs
+++ giocoMobile/Assets/FirstEnemySkeletonScript.cs
@@ -9,6 +9,15 @@ public class FirstEnemySkeletonScript : MonoBehaviour
     public float distance = 1f;
     public LayerMask layerMask;
     public bool facingLeft = true;
+    public bool inRange = false;
+    public Transform player;
+    public float attackRange = 10f;
+    public float retrieveDistance = 2.5f;
+    public float chaseSpeed = 4f;
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRadius = 1f;
+    public LayerMask attackLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -19,26 +28,75 @@ public class FirstEnemySkeletonScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left*Time.deltaTime*moveSpeed);
-
-        RaycastHit2D hit = Physics2D.Raycast(checkPoint.position,Vector2.down,distance,layerMask) ;
-
-        if (!hit && facingLeft)
+        if (Vector2.Distance(transform.position, player.position) <= attackRange)
         {
-            transform.eulerAngles = new Vector3 (0,-180,0);
-            facingLeft = false;
+            inRange = true;
         }
-        else if(!hit && !facingLeft)
+        else
         {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            facingLeft = true;
+            inRange = false;
+        }
+
+        if (inRange)
+        {
+            if (player.position.x > transform.position.x && facingLeft) {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                facingLeft = false;
+            }
+            else if (player.position.x < transform.position.x && !facingLeft)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                facingLeft = true;
+            }
+
+            if (Vector2.Distance(transform.position, player.position) > retrieveDistance)
+            {
+                animator.SetBool("Attack", false);
+                transform.position = Vector2.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("Attack", true);
+            }
+        }
+
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+
+            RaycastHit2D hit = Physics2D.Raycast(checkPoint.position, Vector2.down, distance, layerMask);
+
+            if (!hit && facingLeft)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                facingLeft = false;
+            }
+            else if (!hit && !facingLeft)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                facingLeft = true;
+            }
         }
     }
 
+    public void Attack()
+    {
+        Collider2D collInfo = Physics2D.OverlapCircle(attackPoint.position, attackRadius, attackLayer);
+        if (collInfo)
+        {
+            Debug.Log(collInfo.transform.name);
+        }
+    }
     private void OnDrawGizmosSelected()
     {
         if (checkPoint == null) {  return; }
         Gizmos.color = Color.yellow;
         Gizmos.DrawRay(checkPoint.position,Vector2.down * distance);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,attackRange);
+        if(attackPoint == null) { return;}
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(attackPoint.position,attackRadius);  
     }
 }
